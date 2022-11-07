11b36f7323aa0278ed65deb5b6890c8e3a6b4c36
baraaj <jeridibaraa@gmail.com>
2022-11-07 13:32:17 +0100

    app commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <View style={styles.view2style}>
+      <Text style={{color:"white",textAlign:"center",fontSize:32,fontWeight:"bold",margin:"20"}}>Authentification</Text>
+      <TextInput style={styles.TextInput} placeholder='Username@site.com'></TextInput>
+      <TextInput style={styles.TextInput} placeholder='Password'></TextInput>
+      </View>
     </View>
   );
 }
@@ -13,8 +17,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  view2style:{
+    backgroundColor:"#0005",
+    height:400,
+    width:"90%",
+    borderRadius:10,
+    alignItems:"center"
+  },
+  TextInput:{
+    margin:10,
+    color:"black",
+    height:80,
+    width:"80%",
+    backgroundColor:"white",
+    alignItems:"center",
+    borderRadius:5,
+  }
 });
